485e6801b5e77cd909c1ee7647c2c4ad50a641bd
= <=>
2023-01-13 11:24:29 +0530

    13jan commit

--- js/script.js
+++ js/script.js
@@ -31,12 +31,19 @@ form.addEventListener('submit',event=>{
 
 function showTodo(todo){
 
+    const item = document.querySelector(`[data-key='${todo.id}']`);
+
+    if (todo.deleted) {
+        item.remove();
+        return
+      }
+
     localStorage.setItem('todoRef', JSON.stringify(toDos));
     const list = document.querySelector('.list');
     const isChecked = todo.checked ? 'done' : '';
 
     const li = document.createElement("li");
-    li.setAttribute('class', `todoClass${isChecked}`);
+    li.setAttribute('class', `todoClass`);
 
     li.setAttribute('data-key', todo.id);
 
@@ -44,20 +51,49 @@ function showTodo(todo){
     <span id="${todo.id}" class="unchecked"> - </span>
     <span id="${todo.id}" class="checked"> &#10003 </span>
     <label id="${todo.id}" class="task">${todo.name}</label></div>
-    <i class="fa fa-trash" style="color: red;" onclick="remove(this)"></i>`;
+    <i class="fa fa-trash delete" style="color: red;"></i>`;
 
     list.append(li);
 }
 
-function remove(event) {
-    for(let i=0;i<toDos.length;i++){
-        if(event == toDos[i])
-        {
-            toDos.splice(i,1);
-        }
+// function remove(event) {
+//     let stored = localStorage.getItem("todoRef");
+//     let toDos = JSON.parse(stored);
+//     for(let i=0;i<toDos.length;i++){
+//         if(event == toDos[i])
+//         {
+//             toDos.splice(i,1);
+//         }
+//     }
+//     event.parentElement.remove();
+//     localStorage.setItem("todoRef", JSON.stringify(toDos));
+// }
+
+document.addEventListener('click', function deleteHandler(event){
+    var hasClass = event.target.matches('.delete');
+    if(hasClass){
+        const itemKey = event.target.parentElement.dataset.key;
+        deleteTodo(itemKey);
     }
-    event.parentElement.remove();
-}
+})
+
+function deleteTodo(key) {
+    
+    let stored = localStorage.getItem("todoRef");
+    toDos = JSON.parse(stored);
+
+    const index = toDos.findIndex(item => item.id === Number(key));
+    
+    const todo = {
+      deleted: true,
+      ...toDos[index]
+    };
+    
+    toDos = toDos.filter(item => item.id !== Number(key));
+    localStorage.setItem("todoRef", JSON.stringify(toDos));
+    showTodo(todo);
+  }
+
 document.addEventListener('keyup', keypress);
 function keypress(e){
     e.preventDefault()
