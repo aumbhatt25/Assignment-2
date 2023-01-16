5b5643befb027bed02e4ae0209f71ebf1751ed17
= <=>
2023-01-16 18:09:18 +0530

    13jan commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 let toDos = [];
-
 const form = document.querySelector('form');
 
 form.addEventListener('submit',event=>{
@@ -11,12 +10,13 @@ form.addEventListener('submit',event=>{
         addToDo(text);
         input.value = '';
     }
-    document.addEventListener('keyup', keypress);
+    if(document.addEventListener('keyup', keypress)){
     function keypress(e){
         e.preventDefault()
         if (e.key === 'Enter' || e.keyCode === 13) {
             addToDo(text);
         }
+    }
     }
 });
 
@@ -26,75 +26,86 @@ function addToDo(text){
         checked: false,
         id: Date.now()
     };
-    
     if(todo.name.value==''){
         todo.name.value.remove();
     }
 
     toDos.push(todo);
     console.log(toDos);
-    showTodo(toDos);
+
     localStorage.setItem('todoRef', JSON.stringify(toDos));
+    showTodo(toDos);
 }
 
-function showTodo(toDos){
+function showTodo(array){
 
     const list = document.querySelector('.list');
 
+    array.forEach(function(todo){
 
-    toDos.forEach(function(todo){
+        const item = document.querySelector(`[data-key='${todo.id}']`);
 
         const isChecked = todo.checked ? 'done' : '';
         const li = document.createElement("li");
-        li.setAttribute('class', `todoClass`);
+        li.setAttribute('class', `todoClass ${isChecked}`);
 
         li.setAttribute('data-key', todo.id);
-
-        if(todo.checked == true){
-            li.classList.add('checked');
-        }
 
         li.innerHTML = `<div class="left"><input type="checkbox" id="${todo.id}" name="checkbox" class="check" onclick="checkingCheckBox(${todo.id})">
         <span id="${todo.id}" class="unchecked"> - </span>
         <span id="${todo.id}" class="checked"> &#10003 </span>
         <label id="${todo.id}" class="task">${todo.name}</label></div>
-        <i class="fa fa-trash delete" style="color: red;"></i>`;
+        <i class="fa fa-trash delete" onclick="deleteTodo(${todo.id})" style="color: red;"></i>`;
 
-        list.append(li);
+        if (item) {
+            list.replaceChild(li, item);
+          } else {
+            list.append(li);
+          }
     });
 }
 
     // let myList = document.querySelector('ul')
-    document.addEventListener('click', function deleteHandler(event){
-    var hasClass = event.target.matches('.delete');
-    if(hasClass){
-        const itemKey = event.target.parentElement.dataset.key;
-        deleteTodo(itemKey);
-    }
-})
+//     document.addEventListener('click', function deleteHandler(event){
+//     var hasClass = event.target.matches('.delete');
+//     if(hasClass){
+//         const itemKey = event.target.parentElement.dataset.key;
+//         deleteTodo(itemKey);
+//     }
+// })
 
-function deleteTodo(key) {
-    
-    let stored = localStorage.getItem("todoRef");
-    toDos = JSON.parse(stored);
+// function deleteTodo(key) {
+    // 
+    // let stored = localStorage.getItem("todoRef");
+    // toDos = JSON.parse(stored);
 
-    const index = toDos.findIndex(item => item.id === Number(key));
+    // const index = toDos.findIndex(item => item.id === Number(key));
     
-    const todo = {
-      deleted: true,
-      ...toDos[index]
-    };
+    // const todo = {
+    //   deleted: true,
+    //   ...toDos[index]
+    // };
     
-    toDos = toDos.filter(item => item.id !== Number(key));
-    localStorage.setItem("todoRef", JSON.stringify(toDos));
+    // toDos = toDos.filter(item => item.id !== Number(key));
+    // localStorage.setItem("todoRef", JSON.stringify(toDos));
     
-    const item = document.querySelector(`[data-key='${todo.id}']`);
+    // const item = document.querySelector(`[data-key='${todo.id}']`);
 
-    if (todo.deleted) {
-        item.remove();
-        return;
-      }
-  }
+    // if (todo.deleted) {
+    //     item.remove();
+    //     return;
+    //   }
+//   }
+
+function deleteTodo(id){
+    const ref = localStorage.getItem('todoRef');
+    console.log(ref);
+    let tempArr = JSON.parse(ref);
+      
+    tempArr = tempArr.filter(item => item.id !== Number(id));
+    console.log(id);
+    localStorage.setItem("todoRef", JSON.stringify(tempArr));
+}
 
 const todoInput = document.getElementById("todoInput");
 
@@ -125,13 +136,12 @@ function checkingCheckBox(id){
 
     localStorage.setItem("todoRef", JSON.stringify(tempArr));
 }
+window.addEventListener('change', save);
 
 document.addEventListener('DOMContentLoaded', () => {
     const ref = localStorage.getItem('todoRef');
-    const tempArr = JSON.parse(ref);
     if (ref) {
-      toDos = JSON.parse(ref);
-      showTodo(tempArr);
+        toDos = JSON.parse(ref);
+        showTodo(toDos);
     }
   });
-  
